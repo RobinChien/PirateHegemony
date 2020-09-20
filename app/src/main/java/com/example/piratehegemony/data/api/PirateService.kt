e2357021e0052f35d295ccf214ca6c654d7e841c
robinchien <goodnana1224@gmail.com>
2020-09-20 20:24:18 +0800

    Create api of fetchPirateInfo

--- app/src/main/java/com/example/piratehegemony/data/api/PirateService.kt
+++ app/src/main/java/com/example/piratehegemony/data/api/PirateService.kt
@@ -1,16 +1,23 @@
 package com.example.piratehegemony.data.api
 
+import com.example.piratehegemony.data.model.PirateInfo
 import com.example.piratehegemony.data.model.PirateResponse
-import retrofit2.Call
+import com.skydoves.sandwich.ApiResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface PirateService {
 
     @GET("pokemon")
-    fun fetchPirateList(
+    suspend fun fetchPirateList(
         @Query("limit") limit: Int = 10,
         @Query("offset") offset: Int = 0
-    ): Call<PirateResponse>
+    ): ApiResponse<PirateResponse>
+
+    @GET("pokemon/{id}")
+    suspend fun fetchPirateInfo(
+        @Path("id") id: Int
+    ): ApiResponse<PirateInfo>
 
 }
